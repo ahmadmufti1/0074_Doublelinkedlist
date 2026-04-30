1d87bf2bc2e1fce7cf2fd1a0f9e480bbc3787bd3
ahmadmufti1 <muftiahmad617@gmail.com>
2026-04-30 23:15:50 +0700

    Membuat class Node untuk menyimpan data mahasiswa

--- 0074_Doublelinkedlist.cpp
+++ 0074_Doublelinkedlist.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node { //Membuat class Node untuk menyimpan data mahasiswa
+public:
+    int noMhs;
+    Node* prev;
+    Node* next;
+};
